8029caff653c20ea64ad23192e17c1c53fa764b8
WorldonDynamics <bworldon@gmail.com>
2025-11-23 16:18:42 +1000

    style: run flake8 fixes  clean fences, tests, and gitignore

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,7 +12,7 @@ SRC_PATH = os.path.join(ROOT, "mini-deep-research-agent", "src")
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
 
-import utils as utils_mod
+import utils as utils_mod  # noqa: E402
 
 
 class UtilsTest(unittest.TestCase):
